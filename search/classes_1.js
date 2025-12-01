6dfa088bb2c95a1b16594a09b4806f0f8b0edb20
ArthurCottreau <ArthurCottreau@users.noreply.github.com>
2025-12-01 19:08:36 +0000

    deploy: 6b2c8a2df68d9eadac42a150b1c7d0114c5f2a5b

--- search/classes_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['savedata_0',['SaveData',['../classsave__data_1_1SaveData.html',1,'save_data']]]
-];
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,7 +1,7 @@
 var indexSectionsWithContent =
 {
-  0: "rs",
-  1: "rs"
+  0: "r",
+  1: "r"
 };
 
 var indexSectionNames =
