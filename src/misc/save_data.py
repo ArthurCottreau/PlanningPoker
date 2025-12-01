924abaae7ee6eff20ac5a38053a1a897a7cec737
ArthurCottreau <arthur.cottreau@univ-lyon2.fr>
2025-12-01 12:48:15 +0100

    Add ability to save json file + more checks for issues

--- src/misc/save_data.py
+++ src/misc/save_data.py
@@ -12,16 +12,29 @@ class SaveData:
         try:
             json.loads(myjson)
         except ValueError as e:
+            print("Error! This file is not structured like a JSON!")
             return False
         return True
 
     def load_json(self, path):
-        file_content = ""
-        with open(path) as f:
-            file_content = f.read()
+        if path:
+            try:
+                with open(path) as f:
+                    file_content = f.read()
 
-        if self.is_json(file_content):
-            self.file_data = json.loads(file_content)
+                    if self.is_json(file_content):
+                        self.file_data = json.loads(file_content)                   
+            except Exception as e:
+                print("An Error occured while trying to read the JSON!",e)
+
+    def save_json(self, path):
+        if path:
+            try:
+                with open(path + ".json","w",encoding="utf-8") as f:
+                    save_data = json.dumps(self.file_data)
+                    f.write(save_data)
+            except Exception as e:
+                print("An Error occured while trying to save the JSON!",e)
 
     def set_element(self, element, data):
         self.file_data[element] = data
@@ -30,4 +43,5 @@ class SaveData:
         if element in self.file_data:
             return self.file_data[element]
         else:
+            print("Error! This JSON element doesn't exist!")
             return ""
